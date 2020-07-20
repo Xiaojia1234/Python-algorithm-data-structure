4afc721b510f2fec3d711fc6a4a0c45b150a5999
Xiaojia1234 <1399843193@qq.com>
2020-07-20 14:38:53 +0800

    fib2

--- Dynamic programming/fib.py
+++ Dynamic programming/fib.py
@@ -1,5 +1,8 @@
 # 斐波那契额数列
 
+
+
+# 递归实现
 # 什么地方调用递归本身
 # 什么时候终止递归
 # 1 1 2 3 5 8 13 21 34 55 89
@@ -11,5 +14,24 @@ def fib_test(k):
 
 
 
+
+# 不使用递归方法
+def fib_test2(k):
+    assert k > 0, "k 必须大于0"
+    k_1 = 1
+    k_2 = 1
+    if k in [1,2]:
+        return 1
+    for i in range(3,k+1):
+        i_value = k_1 + k_2
+        temp = k_1
+        k_1 = i_value
+        k_2 = temp
+    return k_1
+
+
+
+
 if __name__=="__main__":
-    print(fib_test(4))
+    print(fib_test(7))
+    print(fib_test2(8))
