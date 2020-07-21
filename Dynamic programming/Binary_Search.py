1eeeee4eb3359f5a23f98f9047ef618cf040c19d
Xiaojia1234 <1399843193@qq.com>
2020-07-21 10:31:27 +0800

    binary search 使用递归

--- Dynamic programming/Binary_Search.py
+++ Dynamic programming/Binary_Search.py
@@ -21,9 +21,24 @@ def search(data_list,target):
             right = mid -1
     return target_pos
 
+
+
+def search2(left, right, data_list, target):
+    mid = int((left + right) / 2)
+    if data_list[mid] == target:
+        return mid
+    elif data_list[mid] < target:
+        # 如果中间值小于目标值，则继续在右侧二分查找
+        return search2(mid+1, right, data_list, target)
+    else:
+        # 如果中间值大于目标值，则在左侧继续查找
+        return search2(left, mid-1, data_list, target)
+
+
 if __name__ == "__main__":
-    pos = search(data,100)
-    if pos > 0 :
+    # pos = search(data,100)
+    pos = search2(0,len(data)-1,data, 29)
+    if pos >= 0 :
         print("查询到数据，所在位置：{}".format(pos))
     else:
         print("目标值不在列表中")
