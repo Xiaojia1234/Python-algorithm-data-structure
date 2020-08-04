1f6fbd8023f62cdee7ffe7462b22f5beec7543dd
Xiaojia1234 <1399843193@qq.com>
2020-08-04 11:34:47 +0800

    pyramid_mem

--- Dynamic programming/chapter03/pyramid_mem.py
+++ Dynamic programming/chapter03/pyramid_mem.py
@@ -15,15 +15,26 @@ pyramid = [
 
 
 max_value = 0
-def search_max(depth,y):
-    # 1.把垂直方向下方的值交给下一个人 得到最大值
-    left_max = search_max(depth+1,y)
-    # 2.把垂直方向右下方的值交给下一个人 得到最大值
-    right_max = search_max(depth + 1, y + 1)
-    return pyramid[depth][y] + max(left_max,right_max)
+# def search_max(depth,y):
+#     if depth == 4:
+#         return pyramid[depth][y]
+#     else:
+#         # 1.把垂直方向下方的值交给下一个人 得到最大值
+#         left_max = search_max(depth+1,y)
+#         # 2.把垂直方向右下方的值交给下一个人 得到最大值
+#         # 1.任务可以下分；2.最优子结构，3.决策
+#         right_max = search_max(depth + 1, y + 1)
+#         return pyramid[depth][y] + max(left_max,right_max)
 
+
+# 化简版本
+def search_max(depth,y):
+    if depth == 4:
+        return pyramid[depth][y]
+    else:
+        return pyramid[depth][y] + max(search_max(depth+1,y),search_max(depth + 1, y + 1))
 
 
 
 if __name__ == "__main__":
-    search_max(1,0)
+    print(search_max(3,0))
